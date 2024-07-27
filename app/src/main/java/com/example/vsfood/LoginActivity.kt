26050e02778a7756ac417d59d1b4ed13a4aff1de
vishal <suryawanshivishal1710@gmail.com>
2024-07-27 23:31:04 +0530

    add sign_kt activity

--- app/src/main/java/com/example/vsfood/LoginActivity.kt
+++ app/src/main/java/com/example/vsfood/LoginActivity.kt
@@ -1,16 +1,30 @@
 package com.example.vsfood
 
+import android.content.Intent
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vsfood.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding : ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        binding.loginbutton.setOnClickListener {
+            val intent = Intent (this,SignActivity::class.java)
+            startActivity(intent)
+        }
+        binding.donthavebutton.setOnClickListener {
+            val intent = Intent(this,SignActivity::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/vsfood/SignActivity.kt
+++ app/src/main/java/com/example/vsfood/SignActivity.kt
@@ -1,16 +1,25 @@
 package com.example.vsfood
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vsfood.databinding.ActivitySignBinding
 
 class SignActivity : AppCompatActivity() {
+    private val binding : ActivitySignBinding by lazy {
+        ActivitySignBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign)
+        setContentView(binding.root)
+        binding.alreadyhavebutton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
