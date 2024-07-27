cf4624afc2a2c03185c3c376956b4b159c6b7f76
vishal <suryawanshivishal1710@gmail.com>
2024-07-28 01:19:25 +0530

    add choose location acvtivity add choose location list kotlin file

--- app/src/main/java/com/example/vsfood/ChooseLocationActivity.kt
+++ app/src/main/java/com/example/vsfood/ChooseLocationActivity.kt
@@ -1,29 +1,28 @@
 package com.example.vsfood
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.vsfood.databinding.ActivitySignBinding
+import com.example.vsfood.databinding.ActivityChooseLocationBinding
 
-class SignActivity : AppCompatActivity() {
-    private val binding : ActivitySignBinding by lazy {
-        ActivitySignBinding.inflate(layoutInflater)
+class ChooseLocationActivity : AppCompatActivity() {
+    private val binding:ActivityChooseLocationBinding by lazy {
+        ActivityChooseLocationBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        binding.alreadyhavebutton.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
-        }
-        binding.signbutton.setOnClickListener {
-            val intent = Intent(this,ChooseLocationActivity::class.java)
-            startActivity(intent)
-        }
+        val Locationlist = arrayOf("Pimpri", "Akurdi", "Chinchwad", "Nigdi")
+        val adapter = ArrayAdapter(/* context = */ this,/* resource = */
+            android.R.layout.simple_list_item_1,/* objects = */
+            Locationlist)
+        val autoCompleteTextView = binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
