733a24bfe4b145b495de6962bf70ac3037ef25c7
vishal <suryawanshivishal1710@gmail.com>
2024-07-27 21:06:09 +0530

    add loginxml activity

---
+++ app/src/main/java/com/example/vsfood/Splash_screenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.vsfood
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@Suppress("DEPRECATION")
+class Splash_screenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler().postDelayed({
+            val intent = Intent(this, startActivity ::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
