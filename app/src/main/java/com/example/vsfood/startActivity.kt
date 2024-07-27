5a1388eedc75dba9f727551ffb70ee01ec00bed3
vishal <suryawanshivishal1710@gmail.com>
2024-07-27 18:39:45 +0530

    add start activity

--- app/src/main/java/com/example/vsfood/startActivity.kt
+++ app/src/main/java/com/example/vsfood/startActivity.kt
@@ -1,28 +1,20 @@
 package com.example.vsfood
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-@Suppress("DEPRECATION")
-class splash_screenActivity : AppCompatActivity() {
+class startActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_start)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        Handler().postDelayed({
-            val intent = Intent(this, startActivity ::class.java)
-            startActivity(intent)
-        },2000)
     }
 }
