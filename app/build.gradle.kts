b89ec3a46584c1f5d2895a7f8d1753ad6391c081
vishal <suryawanshivishal1710@gmail.com>
2024-07-28 14:15:45 +0530

    add slider model in home fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.0")
+
 }
--- app/src/main/java/com/example/vsfood/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/vsfood/Fragment/HomeFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.vsfood.R
+import com.example.vsfood.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+    private lateinit var binding : FragmentHomeBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +25,21 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val imagelist = ArrayList<SlideModel>()
+        imagelist.add(SlideModel(R.drawable.banner1,ScaleTypes.FIT))
+        imagelist.add(SlideModel(R.drawable.banner2,ScaleTypes.FIT))
+        imagelist.add(SlideModel(R.drawable.banner3,ScaleTypes.FIT))
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imagelist)
+        imageSlider.setImageList(imagelist,ScaleTypes.FIT)
     }
 
 }
