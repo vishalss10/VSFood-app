4baa10eb5121bf475bc54d6421e41ae9115afb12
vishal <suryawanshivishal1710@gmail.com>
2024-07-30 00:57:14 +0530

    cart recycler vew laylout added

---
+++ app/src/main/java/com/example/vsfood/Adapter/cartAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.vsfood.Adapter
+
+import android.media.RouteListingPreference.Item
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vsfood.databinding.CartItemBinding
+
+class cartAdapter(private val cartItem:MutableList<String>,private val cartItemprice:MutableList<String>,private val cartImage:MutableList<Int>) : RecyclerView.Adapter<cartAdapter.cartviewHolder>() {
+
+private val itemquantity = IntArray(cartItem.size){1}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): cartviewHolder {
+        val binding = CartItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return cartviewHolder(binding)
+    }
+    override fun onBindViewHolder(holder: cartviewHolder, position: Int) {
+        holder.bind(position)
+    }
+    override fun getItemCount(): Int = cartItem.size
+
+   inner class cartviewHolder( private val binding: CartItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+           binding.apply {
+               val quantity = itemquantity[position]
+               cartfoodname.text = cartItem[position]
+               cartitemprice.text = cartItemprice[position]
+               cartimage.setImageResource(cartImage[position])
+               cartquantity.text = quantity.toString()
+
+
+
+           }
+        }
+
+    }
+
+
+}
